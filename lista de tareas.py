29dca9e13b0ae56ab2b456d1be501c4efe561984
Cristhian Urrego Salazar <infoafuro@gmail.com>
2019-05-08 22:09:01 -0500

    avance

--- lista de tareas.py
+++ lista de tareas.py
@@ -2,7 +2,7 @@
 2. uso de emojis y sonidos para notificaciones.
 3. registrar usuarios con foto. terminado (sin foto)
 4. log de usuarios, reporta quien inicia sesion, intentosfallidos, ip, hora. terminado
-5. listado de usuarios conectados.
+5. listado de usuarios conectados. terminado
 6. chat grupal.
 7. chat individual.
 8. limpiar conversacion.
